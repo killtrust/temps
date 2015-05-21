f9c112800c73541bba41c0f008c2396a6595d3d8
triaud <triaud@geitp105-02.insa-toulouse.fr>
2015-05-21 09:58:34 +0200

    Mise_ajour

---
+++ projet_destijl_2014/projet_initial/global.c
@@ -0,0 +1,36 @@
+/*
+ * File:   global.h
+ * Author: pehladik
+ *
+ * Created on 21 avril 2011, 12:14
+ */
+
+#include "global.h"
+
+RT_TASK tServeur;
+RT_TASK tconnect;
+RT_TASK tmove;
+RT_TASK tenvoyer;
+
+RT_MUTEX mutexEtat;
+RT_MUTEX mutexMove;
+RT_MUTEX mutexCpt;
+
+RT_SEM semConnecterRobot;
+
+RT_QUEUE queueMsgGUI;
+
+int etatCommMoniteur = 1;
+int etatCommRobot = 1;
+DRobot *robot;
+DMovement *move;
+DServer *serveur;
+int cpt_perte_com = 0;
+
+
+int MSG_QUEUE_SIZE = 10;
+
+int PRIORITY_TSERVEUR = 30;
+int PRIORITY_TCONNECT = 20;
+int PRIORITY_TMOVE = 10;
+int PRIORITY_TENVOYER = 25;
